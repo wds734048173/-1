ff0e0b26c38e61d02aa5e325b4beb6bef06fe3a7
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-06-20 20:58:59 +0000

    Updates

---
+++ _astro/popup.a359f713.js
@@ -0,0 +1,1 @@
+function e(){const o=document.querySelector("#login-popup");if(!o)return;o.classList.remove("hidden"),o.classList.add("flex");const s=o.querySelector("[autofocus]");s&&s.focus()}export{e as s};
